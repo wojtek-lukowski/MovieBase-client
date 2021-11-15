8fcf4009fb36e6dfd010ad9553a58041f83951c6
Wojtek Lukowski <wlukowski@4qu.de>
2021-11-15 20:45:36 +0100

    current status

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -12,45 +12,45 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 
 export function LoginView(props) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.post('https://moviebased.herokuapp.com/login', {
-            Username: username,
-            Password: password
-        })
-            .then(response => {
-                const data = response.data;
-                props.onLoggedIn(data);
-            })
-            .catch(e => {
-                console.log('No such user.')
-            });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('https://moviebased.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('No such user.')
+      });
+  };
 
-    return (
+  return (
 
-        <Form className="form">
-            <h2>Log In</h2>
-            <Form.Group controlId="formUsername">
-                <Form.Label>Username: </Form.Label>
-                <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username"></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-                <Form.Label>Password: </Form.Label>
-                <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password"></Form.Control>
-            </Form.Group>
-            <div className="form-button">
-                <Button variant="outline-primary" type="submit" onClick={handleSubmit}>Log in</Button>
-            </div>
-        </Form >
-    )
+    <Form className="form">
+      <h2>Log In</h2>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username: </Form.Label>
+        <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username"></Form.Control>
+      </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password: </Form.Label>
+        <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password"></Form.Control>
+      </Form.Group>
+      <div className="form-button">
+        <Button variant="outline-primary" type="submit" onClick={handleSubmit}>Log in</Button>
+      </div>
+    </Form >
+  )
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    handleSubmit: (username, password) => dispatch(handleSubmit(username, password))
+  handleSubmit: (username, password) => dispatch(handleSubmit(username, password))
 });
 
 export default connect(null, mapDispatchToProps)(LoginView);
