185476493ac810e608c0cb7d8a4cb1106eac0b4b
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-02-11 17:16:37 +0100

    addToFavs disabled

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -19,25 +19,25 @@ export default function MovieCard(props) {
     }
   }, [favorites])
 
-  addToFavs = (movieId) => {
-    const username = localStorage.getItem("user");
-    const token = localStorage.getItem("token");
+  // addToFavs = (movieId) => {
+  //   const username = localStorage.getItem("user");
+  //   const token = localStorage.getItem("token");
 
-    axios
-      .post(
-        `https://moviebased.herokuapp.com/users/${username}/movies/` + movieId,
-        {},
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      )
-      .then((response) => {
-        // console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+  //   axios
+  //     .post(
+  //       `https://moviebased.herokuapp.com/users/${username}/movies/` + movieId,
+  //       {},
+  //       {
+  //         headers: { Authorization: `Bearer ${token}` },
+  //       }
+  //     )
+  //     .then((response) => {
+  //       // console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }
 
   removeFromFavs = (movieId) => {
     const username = localStorage.getItem('user');
