12aae722351696f3d9f4e1ecfe079b71f497a88a
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-02-11 20:04:08 +0100

    functions enabled

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -23,65 +23,65 @@ export default function MovieCard(props) {
     const username = localStorage.getItem("user");
     const token = localStorage.getItem("token");
 
-    axios
-      .post(
-        `https://moviebased.herokuapp.com/users/${username}/movies/` + movieId,
-        {},
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      )
-      .then((response) => {
-        // console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    //   axios
+    //     .post(
+    //       `https://moviebased.herokuapp.com/users/${username}/movies/` + movieId,
+    //       {},
+    //       {
+    //         headers: { Authorization: `Bearer ${token}` },
+    //       }
+    //     )
+    //     .then((response) => {
+    //       // console.log(response);
+    //     })
+    //     .catch(function (error) {
+    //       console.log(error);
+    //     });
+    // }
+
+    // removeFromFavs = (movieId) => {
+    //   const username = localStorage.getItem('user');
+    //   const token = localStorage.getItem('token');
+
+    //   axios.delete(`https://moviebased.herokuapp.com/users/${username}/movies/` + movieId, {
+    //     headers: { Authorization: `Bearer ${token}` }
+    //   })
+    //     .then((response) => {
+    //     })
+    //     .catch(function (error) {
+    //       console.log(error);
+    //     });
+    // }
+
+    return (
+      <div className="card">
+        <div className="card-img">
+          <img src={movie.ImagePath} alt="movie poster" />
+        </div>
+        <div className="title">{movie.Title}
+        </div>
+        <div className="movie-card-button">
+          <Link to={`/movies/${movie._id}`} className="button-primary">See more
+          </Link>
+        </div>
+        <div className="card-actions">
+          {isInFavs &&
+            // <div className="button-primary is-in-favs" onClick={() => { setIsInFavs(false); removeFromFavs(movieId) }} >Remove</div>
+            <div className="button-primary is-in-favs" onClick={() => setIsInFavs(false)} >Remove</div>
+          }
+          {!isInFavs &&
+            // <div className="button-primary" onClick={() => { setIsInFavs(true); addToFavs(movieId) }}>Add to favs</div>
+            <div className="button-primary" onClick={() => setIsInFavs(true)}>Add to favs</div>
+          }
+        </div>
+      </div >
+    );
   }
 
-  removeFromFavs = (movieId) => {
-    const username = localStorage.getItem('user');
-    const token = localStorage.getItem('token');
-
-    axios.delete(`https://moviebased.herokuapp.com/users/${username}/movies/` + movieId, {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then((response) => {
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  return (
-    <div className="card">
-      <div className="card-img">
-        <img src={movie.ImagePath} alt="movie poster" />
-      </div>
-      <div className="title">{movie.Title}
-      </div>
-      <div className="movie-card-button">
-        <Link to={`/movies/${movie._id}`} className="button-primary">See more
-        </Link>
-      </div>
-      <div className="card-actions">
-        {isInFavs &&
-          // <div className="button-primary is-in-favs" onClick={() => { setIsInFavs(false); removeFromFavs(movieId) }} >Remove</div>
-          <div className="button-primary is-in-favs" onClick={() => setIsInFavs(false)} >Remove</div>
-        }
-        {!isInFavs &&
-          // <div className="button-primary" onClick={() => { setIsInFavs(true); addToFavs(movieId) }}>Add to favs</div>
-          <div className="button-primary" onClick={() => setIsInFavs(true)}>Add to favs</div>
-        }
-      </div>
-    </div >
-  );
-}
-
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string,
-    Description: PropTypes.string
-  }).isRequired
-};
+  MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      Title: PropTypes.string.isRequired,
+      ImagePath: PropTypes.string,
+      Description: PropTypes.string
+    }).isRequired
+  };
